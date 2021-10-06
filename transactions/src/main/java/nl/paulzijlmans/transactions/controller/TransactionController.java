6145a134f76e0ec42587029da72951d9db513595
Paul Zijlmans <paul.zijlmans@devolksbank.nl>
2021-10-06 18:36:40 +0200

    Added cross origin

--- transactions/src/main/java/nl/paulzijlmans/transactions/controller/TransactionController.java
+++ transactions/src/main/java/nl/paulzijlmans/transactions/controller/TransactionController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import nl.paulzijlmans.transactions.exception.ResourceNotFoundException;
 import nl.paulzijlmans.transactions.model.Transaction;
 import nl.paulzijlmans.transactions.repository.TransactionRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class TransactionController {
