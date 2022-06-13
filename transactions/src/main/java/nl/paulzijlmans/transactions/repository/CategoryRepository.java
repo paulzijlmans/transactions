a6e0ccb21519d261aa673ebe1431d110715c455f
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-06-13 12:41:09 +0200

    Refactor project structure

--- transactions/src/main/java/nl/paulzijlmans/transactions/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package nl.paulzijlmans.transactions.repository;
-
-import nl.paulzijlmans.transactions.model.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-}
