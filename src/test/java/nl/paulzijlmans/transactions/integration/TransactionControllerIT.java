d407abf5b5250c64d9eff80678dffa696cdaf38f
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-06-21 17:20:20 +0200

    Update transaction controller integration test

--- src/test/java/nl/paulzijlmans/transactions/integration/TransactionControllerIT.java
+++ src/test/java/nl/paulzijlmans/transactions/integration/TransactionControllerIT.java
@@ -1,37 +1,36 @@
 package nl.paulzijlmans.transactions.integration;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 @AutoConfigureMockMvc
 @ActiveProfiles("integration")
+@Sql("/it-data.sql")
 public class TransactionControllerIT {
 
   @Autowired
   private MockMvc mockMvc;
 
-  @Sql("/it-data.sql")
-  @Before
-  void init() {
-
-  }
-
   @Test
   void getTransactions() throws Exception {
-    MvcResult result = mockMvc.perform(get("/api/v1/transactions"))
-        .andExpect(status().isOk()).andReturn();
+    mockMvc.perform(get("/api/v1/transactions"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$", hasSize(1)));
   }
 }
--- src/test/resources/it-data.sql
+++ src/test/resources/it-data.sql
@@ -4,5 +4,5 @@ INSERT INTO categories (id, name) VALUES (2, 'Holiday');
 INSERT INTO categories (id, name) VALUES (3, 'Car');
 INSERT INTO categories (id, name) VALUES (4, 'Other');
 
-INSERT INTO transactions (id, description, amount, comment, category_id)
-  VALUES (0, 'Transaction 1', 99.95, 'Comment 1', 0);
+INSERT INTO transactions (id, description, amount, comment)
+  VALUES (0, 'Transaction 1', 99.95, 'Comment 1');
